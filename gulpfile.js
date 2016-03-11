53f16c80bd5eb6ea67c43a89640001e8e1fe4499
Vitalij Khomenko <homenkovit@gmail.com>
2016-03-12 01:53:53 +0300

    First commit. All html & styles for header

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var gulp 				 = require('gulp'),
+		sass 				 = require('gulp-sass'),
+		sourcemaps	 = require('gulp-sourcemaps'),
+		autoprefixer = require('gulp-autoprefixer'),
+		minifycss		 = require('gulp-minify-css'),
+		jade				 = require('gulp-jade'),
+		concat			 = require('gulp-concat'),
+		uglify			 = require('gulp-uglifyjs'),
+		imagemin 		 = require('gulp-imagemin'),
+    pngquant 		 = require('imagemin-pngquant'),
+		browserSync	 = require('browser-sync').create();
+
+gulp.task('browser-sync', ['styles', 'scripts', 'jade'], function() {
+	browserSync.init({
+		server: {
+			 baseDir: "./build"
+		},
+		notify: false
+	});
+});
+
+gulp.task('styles', function() {
+	return gulp.src('./dev/styles/main.sass')
+		.pipe(sourcemaps.init())
+			.pipe(sass())
+			.pipe(autoprefixer({
+				browsers: ['last 15 versions'],
+				cascade: false
+			}))
+			.pipe(minifycss())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./build/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('jade', function() {
+	return gulp.src('./dev/makeups/pages/*.jade')
+		.pipe(jade())
+		.pipe(gulp.dest('./build'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('./dev/scripts/modules/*.js')
+		.pipe(sourcemaps.init())
+			.pipe(concat('main.js'))
+			.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('image', function() {
+	return gulp.src('./dev/assets/img/**/*.*')
+		.pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()],
+      interlaced: true
+    }))
+    .pipe(gulp.dest('./build/img'));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src('./dev/assets/fonts/**/*.*')
+		.pipe(gulp.dest('./build/fonts'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./dev/styles/**/*.sass', ['styles']);
+	gulp.watch('./dev/makeups/**/*.jade', ['jade']);
+	gulp.watch('./dev/scripts/**/*.js', ['scripts']);
+	gulp.watch('./build/js/*.js').on('change', browserSync.reload);
+	gulp.watch('./build/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['fonts', 'image', 'browser-sync', 'watch']);
